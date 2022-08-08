a4addd6c021c43ba48b9aa145aefe4a3d90e6f49
Zawadi <zawadi.opoti@student.moringaschool.com>
2022-08-08 22:02:21 +0300

    assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code here
+const dodger = document.getElementById('dodger')
+const moveDodgerLeft = () => {
+    const left = '-180px'
+    dodger.style.left = left;
+};
+const moveDodgerRight = () => {
+    const left = '180px';
+    dodger.style.left = left;
+}
